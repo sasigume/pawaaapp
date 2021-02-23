c20a3e9406920674397fc009fa7ba842b185d573
Ryo Ando <sasigume@gmail.com>
2021-02-23 23:16:36 +0900

    added repo url on footer

--- src/components/partials/layout/index.tsx
+++ src/components/partials/layout/index.tsx
@@ -2,6 +2,7 @@ import Nav from './nav'
 import Meta from './meta'
 import { ReactNode } from 'react'
 import { CREATOR_ID } from '@/lib/constants'
+import LayoutFooter from './footer'
 
 interface LayoutProps {
   preview: boolean;
@@ -14,24 +15,14 @@ export default function Layout({ preview, children, title, desc }: LayoutProps) 
   return (
     <div>
       <Meta title={title} desc={desc} />
-      <div className="w-screen flex flex-col items-center justify-start overflow-hidden min-h-screen pb-12">
+      <div className="w-screen flex flex-col items-center justify-start overflow-hidden min-h-screen">
         <Nav preview={preview} />
-        <main>          
+        <main>
           {children}
         </main>
-        <div className="px-3">
-          <div>{preview ? (
-            <>
-              プレビューモード: 下書きが表示されています
-            </>
-          ) : (
-              <>
-                <a className="underline" href={(`https://twitter.com/${CREATOR_ID}`)}>不具合の報告</a>
-              </>
-            )}</div>
-          <div>管理人はメアドを見れます。ご注意ください。</div>
-          <div>&copy; 2021 Ryo Ando (@{CREATOR_ID}) / Built with <a href="https://www.storyblok.com">Storyblok</a></div>
-        </div>
+
+        <LayoutFooter />
+
       </div>
     </div>
   )
