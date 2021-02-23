83a024ac3d28cbf37fa9710e5d66d39429bc1618
Ryo Ando <sasigume@gmail.com>
2021-02-23 23:17:11 +0900

    updated warning

--- src/components/common/warning.tsx
+++ src/components/common/warning.tsx
@@ -1,8 +1,11 @@
 import Link from "next/link"
 
-const Warning =()=>(
-  <div className="bg-red-500 text-white text-xl font-bold p-4 m-4">
-    <Link href="/posts/eula" ><a className="underline">利用規約</a></Link>に反した投稿は即刻削除します。Twitterアカウントと投稿が紐づけられていることを忘れないでください。
-    </div>
+const Warning = () => (
+  <div className="bg-gray-500 text-white text-lg font-bold p-4 m-4">
+    <div>
+      <Link href="/posts/eula" ><a className="underline">利用規約</a></Link>に反した投稿は即刻削除します。Twitterアカウントと投稿が紐づけられていることを忘れないでください。
+      </div>
+    <div>サーバーサイドのデータは常時同期しているわけではないので、コメントなどの反映には少し時間がかかります。</div>
+  </div>
 )
 export default Warning
