478ede87260691b322d92bf6d9635ba58d3e3370
Ryo Ando <sasigume@gmail.com>
2021-02-27 16:51:27 +0900

    added markdown for top page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -41,7 +41,7 @@ const Index = ({ page, environment }: IndexProps) => {
           )}
       </>) : (
           <Layout preview={environment} title={page.title} desc={page.description}>
-            <div className="w-screen flex">
+            <div className="w-screen flex mb-6">
               <Container>
                 <div className="max-w-3xl flex flex-col items-center justify-center">
                   <div className="flex flex-col align-middle items-center md:flex-row justify-between pt-12">
@@ -51,13 +51,13 @@ const Index = ({ page, environment }: IndexProps) => {
                     <div className="flex flex-col text-black py-8 relative pt-32">
                       <div className="flex flex-col z-20 mt-4">
                         {(page.postsCollection && page.postsCollection.items.length > 0) && page.postsCollection.items.map(
-                        (post: LandingPagePost, n: number) => <LandingPagePostComponent key={post.slug} post={post} n={n} />
-                      )}
+                          (post: LandingPagePost, n: number) => <LandingPagePostComponent key={post.slug} post={post} n={n} />
+                        )}
                       </div>
                       <div className="w-full absolute top-0">
-                        <div className="mx-auto rounded-xl shadow-2xl overflow-hidden" style={{width: "300px", height: "480px"}}>
+                        <div className="mx-auto rounded-xl shadow-2xl overflow-hidden" style={{ width: "300px", height: "480px" }}>
                           <Image src={page.topImage.url} width="300" height="480" />
-                          </div>
+                        </div>
                       </div>
                     </div>
                   </div>
@@ -66,16 +66,21 @@ const Index = ({ page, environment }: IndexProps) => {
             </div>
             <div className="bg-white w-screen flex items-center justify-center">
               <Container>
-                <div className="mb-16">
-                  <h1><Logo /></h1>
-                </div>
+                <div className="w-max-3xl mx-auto text-center">
+                  <div className="mb-16">
+                    <h1><Logo /></h1>
+                  </div>
+                  <div className="mb-16">
+                    <MarkdownRender source={page.md} />
+                  </div>
 
-                <div className="grid gap-x-10 md:grid-flow-col md:auto-cols-max">
+                  <div className="grid gap-x-10 md:grid-flow-col md:auto-cols-max">
 
-                  {(page.screenshotsCollection && page.screenshotsCollection.items.length > 0) && page.screenshotsCollection.items.map(
-                    (sc: Screenshot) => <Mockup key={sc.url} src={sc.url} />
-                  )}
+                    {(page.screenshotsCollection && page.screenshotsCollection.items.length > 0) && page.screenshotsCollection.items.map(
+                      (sc: Screenshot) => <Mockup key={sc.url} src={sc.url} />
+                    )}
 
+                  </div>
                 </div>
               </Container>
             </div>
