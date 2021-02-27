d9251705d94d0e8d464c5d81b88de0b64dc27a95
Ryo Ando <sasigume@gmail.com>
2021-02-28 01:53:13 +0900

    fixed book list design

--- src/components/partials/book-list/book/index.tsx
+++ src/components/partials/book-list/book/index.tsx
@@ -14,17 +14,18 @@ export function BookComponent({ book, mode }: Props) {
   return (
     <article>
       <div className="p-6 rounded-2xl shadow-xl flex">
-        <div className="mr-6 w-64">
+        <div className="mr-6 w-min-64 flex-grow-0">
           <CoverImageComponent slug={book.slug} title={book.title} url={book.coverImage ? book.coverImage.url : ''} />
         </div>
-        <div>
-          <h3 className="text-3xl font-bold mb-3">
+        <div className="flex-grow">
+          <h3 className="text-xl font-bold mb-3">
             <Link as={`/books/${book.slug}`} href="/books/[slug]">
               <a className="hover:underline">{book.title}</a>
             </Link>
           </h3>
-          <div className="text-lg mb-3">
-            公開: {dayjs(book.sys.firstPublishedAt).format('YYYY/MM/DD HH:mm:ss')} /最終更新: {dayjs(book.sys.publishedAt).format('YYYY/MM/DD HH:mm:ss')}
+          <div className="text-sm">
+            <div>公開: {dayjs(book.sys.firstPublishedAt).format('YYYY/MM/DD HH:mm:ss')}</div>
+            <div>最終更新: {dayjs(book.sys.publishedAt).format('YYYY/MM/DD HH:mm:ss')}</div>
           </div>
         </div>
       </div>
@@ -35,8 +36,8 @@ export function BookComponent({ book, mode }: Props) {
 export function SingleBookComponent({ book }: Props) {
   return (
     <article>
-      <div className="px-6 mb-12">
-        <div className="mb-5">
+      <div className="px-6 mb-12 flex flex-col ">
+        <div className="mb-5 max-w-lg mx-auto overflow-hidden flex items-center">
           <CoverImageComponent slug={book.slug} title={book.title} url={book.coverImage ? book.coverImage.url : ''} />
         </div>
         <h1 className="text-3xl font-bold mb-6">
