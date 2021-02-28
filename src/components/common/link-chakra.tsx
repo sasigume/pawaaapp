707c23ff6718849d24926d7193941434449f9873
Ryo Ando <sasigume@gmail.com>
2021-02-28 12:50:30 +0900

    added underline option to link-chakra

--- src/components/common/link-chakra.tsx
+++ src/components/common/link-chakra.tsx
@@ -4,10 +4,16 @@ import React from 'react';
 import NextLink from 'next/link';
 import { Link, LinkProps } from '@chakra-ui/react';
 
-const LinkChakra: React.FC<LinkProps> = (props) => {
+interface LinkChakraProps extends LinkProps {
+  underline?: boolean;
+}
+
+const LinkChakra: React.FC<LinkChakraProps> = (props) => {
+  let textDec
+  props.underline ? textDec = 'underline' : textDec = 'none'
   return (
     <NextLink href={props.href ?? ''}>
-      <Link {...props} style={{textDecoration: 'none'}} onClick={() => (document.activeElement as HTMLElement).blur()} />
+      <Link {...props} style={{ textDecoration: textDec}} onClick={() => (document.activeElement as HTMLElement).blur()} />
     </NextLink>
   );
 };
--- src/components/common/warning.tsx
+++ src/components/common/warning.tsx
@@ -3,7 +3,7 @@ import LinkChakra from "@/components/common/link-chakra"
 const Warning = () => (
   <div className="bg-gray-500 text-white text-lg font-bold p-4 m-4">
     <div>
-      <LinkChakra href="/posts/eula" >利用規約</LinkChakra>に反した投稿は即刻削除します。Twitterアカウントと投稿が紐づけられていることを忘れないでください。
+      <LinkChakra href="/posts/eula" underline={true}>利用規約</LinkChakra>に反した投稿は即刻削除します。Twitterアカウントと投稿が紐づけられていることを忘れないでください。
       </div>
     <div>サーバーサイドのデータは常時同期しているわけではないので、コメントなどの反映には少し時間がかかります。</div>
   </div>
