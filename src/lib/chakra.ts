63cb30bb59698333761c02fdfe4f683ad2f1ef9a
Ryo Ando <sasigume@gmail.com>
2021-02-28 16:08:40 +0900

    added text styles for headings

---
+++ src/lib/chakra.ts
@@ -0,0 +1,23 @@
+export const EXTEND_CHAKRA = {
+  textStyles: {
+    h1: {
+      // you can also use responsive styles
+      fontSize: ["48px", "72px"],
+      fontWeight: "bold",
+      lineHeight: "110%",
+      letterSpacing: "-2%",
+    },
+    h2: {
+      fontSize: ["36px", "48px"],
+      fontWeight: "semibold",
+      lineHeight: "110%",
+      letterSpacing: "-1%",
+    },
+    h3: {
+      fontSize: ["28px", "32px"],
+      fontWeight: "semibold",
+      lineHeight: "110%",
+      letterSpacing: "-1%",
+    },
+  },
+}
