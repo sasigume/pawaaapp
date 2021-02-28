7cde8fc8aa924572b1c7aa28f8f59ab1a995f522
Ryo Ando <sasigume@gmail.com>
2021-02-28 16:08:12 +0900

    markdown now accepts html

--- src/components/partials/post-list/post/post-body.tsx
+++ src/components/partials/post-list/post/post-body.tsx
@@ -3,19 +3,11 @@ interface Props {
   md: string;
 }
 export default function PostBody({ md }: Props) {
-  let context
-  if (md.includes('<p>')) {
-    context = <div dangerouslySetInnerHTML={{ __html: md }} />
-  } else {
-    context = <>
-    <MarkdownRender source={md} />
-    </>
-  }
   return (
     <div className="px-4 text-left lg:w-auto mx-auto">
       <div
         className="overflow-hidden globalStyle_content mx-auto" style={{maxWidth: '650px'}}>
-        {context}
+        <MarkdownRender source={md} />
       </div>
     </div>
   )
