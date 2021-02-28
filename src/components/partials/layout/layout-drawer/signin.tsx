00c784d0e296d52ec1c88d52558613a72f4fefb7
Ryo Ando <sasigume@gmail.com>
2021-02-28 13:44:21 +0900

    changed icons to component

--- src/components/partials/layout/layout-drawer/signin.tsx
+++ src/components/partials/layout/layout-drawer/signin.tsx
@@ -5,6 +5,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Button, Stack } from "@chakra-ui/react"
 import Image from 'next/image'
 import LinkChakra from '@/components/common/link-chakra';
+import FaiconDiv from '@/components/common/faicon-div';
 
 
 const SignIn = () => {
@@ -16,9 +17,6 @@ const SignIn = () => {
   const logout = () => {
     firebaseApi.auth().signOut();
   }
-  const twitterIcon = (<div className="w-5">
-    <FontAwesomeIcon icon={['fab', 'twitter']} />
-  </div>)
 
   return (
     <Stack direction="column" spacing={4} mb={4}>
@@ -26,7 +24,7 @@ const SignIn = () => {
         <Button leftIcon={<div className="w-8 h-8 my-1 mr-2 rounded-full overflow-hidden"><Image src={user.photoUrl} width={32} height={32} /></div>}>
           {user.name}
         </Button>
-        <Button onClick={logout} leftIcon={twitterIcon} colorScheme="red" variant="solid">
+        <Button onClick={logout} leftIcon={<FaiconDiv icon={['fas','twitter']} />} colorScheme="red" variant="solid">
           ログアウト
         </Button></>
       ) : (<>
@@ -34,7 +32,7 @@ const SignIn = () => {
           <div className="w-8 h-8 my-1 mr-2 rounded-full overflow-hidden"><Image src={process.env.HTTPS_URL + '/favicon.png'} width={32} height={32} /></div>
           <div>ログインしていません</div>
         </Button>
-        <Button onClick={login} leftIcon={twitterIcon} colorScheme="twitter" variant="solid">
+        <Button onClick={login} leftIcon={<FaiconDiv icon={['fas','twitter']} />} colorScheme="twitter" variant="solid">
           ログイン
           </Button>
       </>
