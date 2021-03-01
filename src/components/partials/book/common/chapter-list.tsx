f2b562f8dc79fecfbe7573d1ccec0e5e18921d07
Ryo Ando <sasigume@gmail.com>
2021-03-01 10:47:07 +0900

    removed border of last child

--- src/components/partials/book/common/chapter-list.tsx
+++ src/components/partials/book/common/chapter-list.tsx
@@ -17,11 +17,11 @@ interface ListProps {
 const OneBookChapter = ({ bookChapter, bookSlug, num }: Props) => {
   return (
     <LinkChakra href={(`/books/${bookSlug}/chapters/${num + 1}`)}>
-      <Box mb={4} p={3} borderBlockEnd="solid" borderBlockEndColor="gray.100">
-        <Badge colorScheme="green">CHAPTER {num + 1}</Badge>
-        <Box textStyle="h4"><h2>{bookChapter.title}</h2></Box>
-        <div>{bookChapter.description ?? '説明文がありません'}</div>
-      </Box>
+
+      <Badge colorScheme="green">CHAPTER {num + 1}</Badge>
+      <Box textStyle="h4"><h2>{bookChapter.title}</h2></Box>
+      <div>{bookChapter.description ?? '説明文がありません'}</div>
+
     </LinkChakra>
   )
 }
@@ -29,7 +29,19 @@ const OneBookChapter = ({ bookChapter, bookSlug, num }: Props) => {
 const BookChapterList = ({ bookChapters, bookSlug }: ListProps) => {
   return (
     <Flex direction="column">
-      {bookChapters.map((c: BookChapter, num: number) => <OneBookChapter bookSlug={bookSlug} bookChapter={c} key={c.title} num={num} />)}
+      {bookChapters.map((c: BookChapter, num: number, arr: any) => {
+        // if it's last chapter
+        if (arr.length - 1 === num) {
+          return (<Box mb={4} p={3} pt={0}>
+            <OneBookChapter bookSlug={bookSlug} bookChapter={c} key={c.title} num={num} />
+          </Box>)
+        } else {
+          return (<Box mb={4} p={3} borderBlockEnd="solid" borderBlockEndColor="gray.100">
+            <OneBookChapter bookSlug={bookSlug} bookChapter={c} key={c.title} num={num} />
+          </Box>)
+        }
+      }
+      )}
     </Flex>
   )
 }
