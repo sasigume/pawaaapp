37eb7b5fa99f8e5d8450d15194827a0251dc13a5
Ryo Ando <sasigume@gmail.com>
2021-03-02 19:57:26 +0900

    rss/sitemap now contain chapters

--- src/lib/rss/index.ts
+++ src/lib/rss/index.ts
@@ -2,6 +2,7 @@ import fs from "fs"
 
 import { Book } from '@/models/contentful/Book'
 import { SITE_NAME, SITE_DESC } from '../constants'
+import { BookChapter } from "@/models/contentful/BookChapter";
 
 const escapeString = (unsafe: string) => {
   return unsafe
@@ -12,15 +13,34 @@ const escapeString = (unsafe: string) => {
     .replace(/'/g, "&#039;");
 }
 
+const generateChapterItem = (book: Book, chapter: BookChapter, num: number): string => {
+  return (`
+<item>
+    <guid>${process.env.HTTPS_URL}/books/${book.slug}/chapters/${num + 1}</guid>
+    <title>${escapeString(chapter.title)} | ${book.title}</title>
+    <guid>${process.env.HTTPS_URL}/books/${book.slug}/chapters/${num + 1}</guid>
+    <pubDate>${new Date(chapter.sys.publishedAt ?? '').toUTCString()}</pubDate>
+    <summary>${chapter.description ?? '説明文なし'}</summary>
+</item>
+    `)
+}
+
 const generateBookItem = (book: Book): string => {
+  let chapters: string[] = []
+  if (book.chaptersCollection.items && book.chaptersCollection.items.length > 0) {
+    chapters = book.chaptersCollection.items.map(
+      (c: BookChapter, n: number) => generateChapterItem(book, c, n)
+    )
+  }
   return (`
 <item>
     <guid>${process.env.HTTPS_URL}/books/${book.slug}</guid>
     <title>${escapeString(book.title)}</title>
     <link>${process.env.HTTPS_URL}/books/${book.slug}</link>
     <pubDate>${new Date(book.sys.publishedAt ?? '').toUTCString()}</pubDate>
-    <summary>${book.description}</summary>
+    <summary>${book.description ?? "説明文なし"}</summary>
 </item>
+${chapters.length > 0 && chapters.join('')}
     `)
 }
 
