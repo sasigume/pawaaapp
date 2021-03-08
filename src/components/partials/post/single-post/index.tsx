1dd0b5596070a92041471ef57eca8efc753fb240
Ryo Ando <sasigume@gmail.com>
2021-03-08 18:36:59 +0900

    splited post component for list

--- src/components/partials/post/single-post/index.tsx
+++ src/components/partials/post/single-post/index.tsx
@@ -9,23 +9,6 @@ import MarkdownRender from '@/components/common/MarkdownRender'
 interface Props {
   post: Post
 }
-export function PostComponent({ post }: Props) {
-  return (
-    <Flex rounded="xl" shadow="lg" p={0} alignItems="center" area-label={post.title}>
-      <Box flexGrow={1}>
-        <Box textStyle="h4" mb={2}>
-          <LinkChakra href={`/${post.slug}`}>
-            {post.title}
-          </LinkChakra>
-        </Box>
-        <Box area-label="更新日時">
-          <Badge colorScheme="blue">公開: {dayjs(post.sys.firstPublishedAt).format('YYYY/MM/DD')}</Badge>
-          <Badge colorScheme="green">最終更新: {dayjs(post.sys.publishedAt).format('YYYY/MM/DD')}</Badge>
-        </Box>
-      </Box>
-    </Flex>
-  )
-}
 
 export function SinglePostComponent({ post }: Props) {
   return (
