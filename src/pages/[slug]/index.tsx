269c573508cdbc98c0f22af2df3cc0f8f9f6f49c
Ryo Ando <74000913+sasigume@users.noreply.github.com>
2021-03-08 16:32:37 +0900

    Disabled post comments

--- src/pages/[slug]/index.tsx
+++ src/pages/[slug]/index.tsx
@@ -170,8 +170,8 @@ interface GSProps {
 export async function getStaticProps({ params, preview }: GSProps) {
 
   const posts = await getPostAndMorePosts(params.slug, preview)
-  const commentsRes = await fetch(process.env.HTTPS_URL + `/api/postComments/${params.slug}`)
-  const postComments = await commentsRes.json()
+  //const commentsRes = await fetch(process.env.HTTPS_URL + `/api/postComments/${params.slug}`)
+  //const postComments = await commentsRes.json()
 
   const searchWord = SITE_URL + '/' + params.slug
 
@@ -184,7 +184,7 @@ export async function getStaticProps({ params, preview }: GSProps) {
     props: {
       preview: preview ?? false,
       firstPost: posts.post ?? null,
-      postComments: postComments ?? null,
+      //postComments: postComments ?? null,
       morePosts: posts.morePosts ?? null,
       tweetCount: tweetCount ?? null
     },
