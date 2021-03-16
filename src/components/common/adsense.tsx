1dc8d1a469b106525afc6b5cfbaaa456653d4cca
Ryo Ando <sasigume@gmail.com>
2021-03-16 16:24:32 +0900

    (v2.5.0) removed ad slot env and updated adsense component

--- src/components/common/adsense.tsx
+++ src/components/common/adsense.tsx
@@ -1,8 +1,12 @@
 import { Box } from '@chakra-ui/layout';
 import React, { useEffect } from 'react';
 
+interface AdsenseProps{
+  slot: string
+}
+
 // https://qiita.com/qrusadorz/items/14972b6e069feaf777a9
-export default function Adsense(props: any) {
+export default function Adsense(props:AdsenseProps) {
   const enableAd = process.env.ENABLE_AD ?? false
 
   useEffect(() => {
@@ -18,7 +22,7 @@ export default function Adsense(props: any) {
       <ins className="adsbygoogle"
         style={{ "display": "block" }}
         data-ad-client={process.env.GOOGLE_AD_CLIENT}
-        data-ad-slot={process.env.GOOGLE_AD_SLOT}
+        data-ad-slot={props.slot}
         data-ad-format="auto"
         data-full-width-responsive="true"></ins>
         </>) : (
