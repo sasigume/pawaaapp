be901e5ed42478c042c6d449fd7a97e8ea765e10
Ryo Ando <sasigume@gmail.com>
2021-03-16 20:43:03 +0900

    (v2.6.1) fixed wrong multiple page ad push

--- src/components/common/adsense.tsx
+++ src/components/common/adsense.tsx
@@ -1,33 +1,37 @@
 import { Box } from '@chakra-ui/layout';
 import React, { useEffect } from 'react';
 
-interface AdsenseProps{
+interface AdsenseProps {
   slot: string
+  currentPath: any
 }
 
 // https://qiita.com/qrusadorz/items/14972b6e069feaf777a9
-export default function Adsense(props:AdsenseProps) {
+export default function Adsense(props: AdsenseProps) {
+
+  // https://mao-tss.medium.com/fix-google-adsense-loading-issues-with-react-f338cbd61ac4
+  const { currentPath } = props
+
   const enableAd = process.env.ENABLE_AD ?? false
 
   useEffect(() => {
-    if (window.adsbygoogle && process.env.NODE_ENV !== "development") {
-      window.adsbygoogle.push({google_ad_client: process.env.GOOGLE_AD_CLIENT,enable_page_level_ads: true});
-    }
-  }, [])
+    window.adsbygoogle = window.adsbygoogle || []
+    window.adsbygoogle.push({})
+  })
 
   return (
-    <Box mx="auto" my={3}>
+    <Box key={currentPath} mx="auto" my={3}>
       {enableAd ? (<>
-      <Box mb={2} color="gray.800">スポンサーリンク</Box>
-      <ins className="adsbygoogle"
-        style={{ "display": "block" }}
-        data-ad-client={process.env.GOOGLE_AD_CLIENT}
-        data-ad-slot={props.slot}
-        data-ad-format="auto"
-        data-full-width-responsive="true"></ins>
-        </>) : (
-          <span>Adsense無効化中</span>
-        )}
+        <Box mb={2} color="gray.800">スポンサーリンク</Box>
+        <ins className="adsbygoogle"
+          style={{ "display": "block" }}
+          data-ad-client={process.env.GOOGLE_AD_CLIENT}
+          data-ad-slot={props.slot}
+          data-ad-format="auto"
+          data-full-width-responsive="true"></ins>
+      </>) : (
+        <span>Adsense無効化中</span>
+      )}
     </Box>
   )
 }
