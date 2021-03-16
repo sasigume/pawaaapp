0b91a28b785095ad60cf9582ab86c9b60889dfaf
Ryo Ando <sasigume@gmail.com>
2021-03-16 20:43:03 +0900

    (v2.6.1) fixed wrong multiple page ad push

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import { AppProps } from 'next/app'
 import { RecoilRoot } from 'recoil'
-import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 import TagManager from 'react-gtm-module'
 import dayjs from 'dayjs'
@@ -11,44 +10,15 @@ dayjs.locale('ja')
 import '@/lib/firebase'
 import 'hooks/authentication'
 import addIcon from '@/lib/fontawesome'
-import * as gtag from '@/lib/gtag'
 import { Chakra } from '@/components/providers/chakra'
 
 function App({ Component, pageProps }: AppProps) {
   addIcon()
 
-  // Google Analytics
-  // https://sunday-morning.app/posts/2020-12-09-nextjs-google-analytics
-  {/*const router = useRouter()
-  useEffect(() => {
-    const handleRouteChange = (url: string) => {
-      gtag.pageview(url)
-
-      // only on client
-      if (typeof window !== 'undefined' && !window.adsbygoogle) {
-        try {
-          window.adsbygoogle = window.adsbygoogle || []
-          window.adsbygoogle.push({ google_ad_client: gtag.ADSENSE_PUB, enable_page_level_ads: true })
-          console.log('Adsense pushed!')
-        } catch (err) {
-          console.log(err);
-        }
-      }
-    }
-    router.events.on('routeChangeComplete', handleRouteChange)
-
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-
-  }, [router.events]) */}
-
   useEffect(()=>{
     TagManager.initialize({ gtmId: process.env.GTM_ID });
-  },[])
-
-
-
+  })
+  
   return (
     <RecoilRoot>
       <Chakra cookies={pageProps.cookies}>
