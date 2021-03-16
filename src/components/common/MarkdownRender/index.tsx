6f0aae9f27122fa1286b8d0982cdad7d89b86dc3
Ryo Ando <sasigume@gmail.com>
2021-03-17 01:03:52 +0900

    (v2.6.5) removed mathjax and renewed md renderer

--- src/components/common/MarkdownRender/index.tsx
+++ src/components/common/MarkdownRender/index.tsx
@@ -1,46 +1,43 @@
-import { Box, Code } from '@chakra-ui/react';
-import ReactMarkdown from 'react-markdown';
-import ReactMarkdownWithHtml from 'react-markdown/with-html'
-import MathJax from 'react-mathjax';
-import RemarkMathPlugin from 'remark-math';
-const gfm = require('remark-gfm')
+import { Box, Button, Code } from '@chakra-ui/react';
+import Markdown from 'markdown-to-jsx'
+import LinkChakra from '../link-chakra';
 
+interface MdLinkProps {
+  url: string
+  title: string
+}
 
+const MdLink = ({ url, title }: MdLinkProps) => (
+  <LinkChakra href="url" area-label={title}>
+    <Box fontWeight="bold" transitionDuration=".3s" shadow="sm" _hover={{shadow:"lg"}} my={4} p={3} rounded="lg" border="solid" borderWidth={1} borderColor="gray.400">
+      <Box textStyle="h4">{title}</Box>
+      {url}
+    </Box>
+  </LinkChakra>
+)
 
-function MarkdownRender(props: any) {
-  const newProps = {
-    ...props,
-    plugins: [
-      RemarkMathPlugin,
-      gfm
-    ],
-    renderers: {
-      ...props.renderers,
-      heading: (props: any) => (
-        <Box textStyle={(`h${props.level}`)}>
-          {props.level == 1 && <h1>{props.children}</h1>}
-          {props.level == 2 && <h2>{props.children}</h2>}
-          {props.level == 3 && <h3>{props.children}</h3>}
-          {props.level == 4 && <h4>{props.children}</h4>}
-          {props.level == 5 && <h5>{props.children}</h5>}
-          {props.level == 6 && <h6>{props.children}</h6>}
-        </Box>),
-      code: (props: any) =>
-        <Code colorScheme="teal">{props.value}</Code>,
-      math: (props: any) =>
-        <MathJax.Node formula={props.value} />,
-      inlineMath: (props: any) =>
-        <MathJax.Node inline formula={props.value} />
-    }
-  };
+const MdCode = ({children}:any) => (<Code whiteSpace="pre-wrap" colorScheme="teal">{children}</Code>)
+
+interface MdProps {
+  source: string
+}
+function MarkdownRender(props: MdProps) {
   // wrap with class for chakra theme
   return (
     <Box w="full" className="mdrenderWrapper">
-      <MathJax.Provider input="tex">
-        <ReactMarkdownWithHtml allowDangerousHtml {...newProps} />
-      </MathJax.Provider>
+      <Markdown children={props.source} options={{
+        slugify: str => str,
+        overrides: {
+          MdLink: {
+            component: MdLink
+          },
+          pre: {
+            component: MdCode
+          }
+        }
+      }} />
     </Box>
-  );
+  )
 }
 
 export default MarkdownRender
