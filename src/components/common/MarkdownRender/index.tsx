6bc2c31497c41a91a48540af3f0c9ad7dcf13ce2
Ryo Ando <sasigume@gmail.com>
2021-03-17 17:09:14 +0900

    (v2.6.8) changed md render to jsx parser

--- src/components/common/MarkdownRender/index.tsx
+++ src/components/common/MarkdownRender/index.tsx
@@ -1,9 +1,11 @@
-import { Box, Code } from '@chakra-ui/react'
-import Markdown from 'markdown-to-jsx'
-import { compiler } from 'markdown-to-jsx'
-import React from 'react'
+import { Badge, Box, Code } from '@chakra-ui/react'
+import ReactMarkdown from 'react-markdown'
+import JsxParser from "react-jsx-parser"
 import LinkChakra from '../link-chakra'
+import React from 'react'
+import Image from 'next/image'
 
+const gfm = require('remark-gfm')
 interface MdLinkProps {
   url: string
   title: string
@@ -18,26 +20,75 @@ const MdLink = ({ url, title }: MdLinkProps) => (
   </LinkChakra>
 )
 
-const MdCode = ({ children }: any) => (<Code whiteSpace="pre-wrap" colorScheme="teal">{children}</Code>)
 
 function MarkdownRender(props: any) {
-  console.log
+
+  // check if JSX has error
+  const [showMd, setShowMd] = React.useState(true)
+
+  const componentTransforms = {
+    MdLink: (props: any) => <MdLink {...props} />,
+  }
+
+
+  const newProps = {
+    source: props.source,
+    plugins: [
+      gfm,
+    ],
+    renderers: {
+      ...props.renderers,
+
+      heading: (props: any) => (
+        <Box textStyle={(`h${props.level}`)}>
+          {props.level == 1 && <h1>{props.children}</h1>}
+          {props.level == 2 && <h2>{props.children}</h2>}
+          {props.level == 3 && <h3>{props.children}</h3>}
+          {props.level == 4 && <h4>{props.children}</h4>}
+          {props.level == 5 && <h5>{props.children}</h5>}
+          {props.level == 6 && <h6>{props.children}</h6>}
+        </Box>),
+      code: (props: any) =>
+        <Code whiteSpace="pre-wrap" colorScheme="teal">{props.value}</Code>,
+      image: (props: any) =>
+        <Image {...props} />
+    }
+  }
+  const newPropsIfValid = {
+    ...newProps,
+    renderers: {
+      ...newProps.renderers,
+      html: (props: any) =>
+        <JsxParser
+          renderError={(e) => <Badge colorScheme="red">{JSON.stringify(e)}</Badge>}
+          // change state to false if JSX has error
+          onError={(e) => { console.log(e); setShowMd(false) }}
+          autoCloseVoidElements
+          jsx={props.value}
+          components={componentTransforms} />
+    }
+  }
+
+  const ErrorMd = () => {
+    if (!showMd) {
+      return <>
+        <Badge colorScheme="red">編集担当者へ: MarkdownをJSXとしてパースできませんでした</Badge>
+        <ReactMarkdown allowDangerousHtml {...newProps} />
+      </>
+    } else {
+      return <>
+        <Badge>編集担当者へ: JSXにエラーなし</Badge>
+        <ReactMarkdown allowDangerousHtml {...newPropsIfValid} />
+      </>
+    }
+  }
+
   // wrap with class for chakra theme
   return (
     <Box w="full" className="mdrenderWrapper">
-     <Markdown children={props.source} options={{
-        slugify: str => str,
-        overrides: {
-          MdLink: {
-            component: MdLink
-          },
-          pre: {
-            component: MdCode
-          }
-        }
-      }} />
+      <ErrorMd />
     </Box>
-  )
+  );
 }
 
 export default MarkdownRender
