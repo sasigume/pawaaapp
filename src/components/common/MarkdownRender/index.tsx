623aee607859b36ee4150a401546c31d0f4ab689
Ryo Ando <sasigume@gmail.com>
2021-03-17 13:14:31 +0900

    using compiler directly

--- src/components/common/MarkdownRender/index.tsx
+++ src/components/common/MarkdownRender/index.tsx
@@ -1,6 +1,8 @@
-import { Box, Code } from '@chakra-ui/react';
+import { Box, Code } from '@chakra-ui/react'
 import Markdown from 'markdown-to-jsx'
-import LinkChakra from '../link-chakra';
+import { compiler } from 'markdown-to-jsx'
+import React from 'react'
+import LinkChakra from '../link-chakra'
 
 interface MdLinkProps {
   url: string
@@ -9,20 +11,21 @@ interface MdLinkProps {
 
 const MdLink = ({ url, title }: MdLinkProps) => (
   <LinkChakra href="url" area-label={title}>
-    <Box fontWeight="bold" transitionDuration=".3s" shadow="sm" _hover={{shadow:"lg"}} my={4} p={3} rounded="lg" border="solid" borderWidth={1} borderColor="gray.400">
+    <Box fontWeight="bold" transitionDuration=".3s" shadow="sm" _hover={{ shadow: "lg" }} my={4} p={3} rounded="lg" border="solid" borderWidth={1} borderColor="gray.400">
       <Box textStyle="h4">{title}</Box>
       {url}
     </Box>
   </LinkChakra>
 )
 
-const MdCode = ({children}:any) => (<Code whiteSpace="pre-wrap" colorScheme="teal">{children}</Code>)
+const MdCode = ({ children }: any) => (<Code whiteSpace="pre-wrap" colorScheme="teal">{children}</Code>)
 
-function MarkdownRender(props:any) {
+function MarkdownRender(props: any) {
+  console.log
   // wrap with class for chakra theme
   return (
     <Box w="full" className="mdrenderWrapper">
-      <Markdown children={props.source} options={{
+      {/*<Markdown children={props.source} options={{
         slugify: str => str,
         overrides: {
           MdLink: {
@@ -32,7 +35,18 @@ function MarkdownRender(props:any) {
             component: MdCode
           }
         }
-      }} />
+      }} /> */}
+      {compiler(props.source, {
+        slugify: str => str,
+        overrides: {
+          MdLink: {
+            component: MdLink
+          },
+          pre: {
+            component: MdCode
+          }
+        }
+      })}
     </Box>
   )
 }
