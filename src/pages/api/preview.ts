69eee19de3c8490d948b311093de491a1fa69ae3
Ryo Ando <sasigume@gmail.com>
2021-03-17 10:59:03 +0900

    (v2.6.7) added preview api

---
+++ src/pages/api/preview.ts
@@ -0,0 +1,27 @@
+import { getPreviewPost } from '@/lib/contentful/graphql'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function preview(req:NextApiRequest, res:NextApiResponse) {
+  const secret = req.query.secret as string
+  const slug = req.query.slug as string
+
+  if (secret !== process.env.CONTENTFUL_PREVIEW_SECRET || !slug) {
+    return res.status(401).json({ message: 'Invalid token' })
+  }
+
+  const post = await getPreviewPost(slug)
+
+  if (!post) {
+    return res.status(401).json({ message: 'Invalid slug' })
+  }
+
+  res.setPreviewData({})
+
+  const url = `/${post.slug}`
+  res.write(
+    `<!DOCTYPE html><html><head><meta http-equiv="Refresh" content="0; url=${url}" />
+    <script>window.location.href = '${url}'</script>
+    </head>`
+  )
+  res.end()
+}
