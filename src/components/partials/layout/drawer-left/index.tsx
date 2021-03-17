113a285d8799f2f5c56bad8466f3b5a86e9ead71
Ryo Ando <sasigume@gmail.com>
2021-03-17 23:38:09 +0900

    merged drawer menu

--- src/components/partials/layout/drawer-left/index.tsx
+++ src/components/partials/layout/drawer-left/index.tsx
@@ -1,6 +1,9 @@
 import { ReactNode, useRef } from 'react'
+import SignIn from './signin'
 import {
+  useColorMode,
   useDisclosure,
+  Box,
   Button,
   Drawer,
   DrawerBody,
@@ -9,8 +12,12 @@ import {
   DrawerOverlay,
   DrawerContent,
   DrawerCloseButton,
+  Stack,
+  Center,
+  Divider,
 } from "@chakra-ui/react"
 import FaiconDiv from '@/components/common/faicon-div'
+import Logo from '@/components/common/Logo'
 
 interface Props {
   preview: boolean;
@@ -18,13 +25,14 @@ interface Props {
 }
 
 export default function DrawerLeft({ children }: Props) {
+  const { colorMode, toggleColorMode } = useColorMode()
   const { isOpen, onOpen, onClose } = useDisclosure()
   const btnRef = useRef(null)
 
   return (
     <>
-      <Button display={{ base: "flex", xl: "none" }} ref={btnRef} colorScheme="blue" leftIcon={<FaiconDiv icon={['fas', 'bars']} />} onClick={onOpen} position="fixed" top={5} left={5}>
-        CONTENTS
+      <Button pr={{base:2,md:4}} ref={btnRef} colorScheme="blue" leftIcon={<FaiconDiv icon={['fas', 'bars']} />} onClick={onOpen} position="fixed" top={5} left={5}>
+        <Box display={{ base: "none", md: "flex" }}>MENU</Box>
       </Button>
       <Drawer
         isOpen={isOpen}
@@ -37,11 +45,28 @@ export default function DrawerLeft({ children }: Props) {
           <DrawerContent>
             <DrawerCloseButton />
             <DrawerHeader>
+              <Divider mb={8} />
+
+              <Stack direction="column" spacing={6} mb={6}>
+                <Button aria-label="カラーモードを切り替える" colorScheme={colorMode === "light" ? "purple" : "cyan"} onClick={toggleColorMode} leftIcon={colorMode === "light" ?
+                  (<FaiconDiv icon={['fas', 'moon']} />) : (<FaiconDiv icon={['fas', 'sun']} />)}>
+                  {colorMode === "light" ? "ダークモード" : "ライトモード"}
+                </Button>
+              </Stack>
+
+              <SignIn />
             </DrawerHeader>
             <DrawerBody overflow-y="scroll">
               {children}
             </DrawerBody>
             <DrawerFooter>
+              <Box>Powered by
+              <Box mb={8}>
+                  <Center>
+                    <Logo fill={colorMode === "light" ? "#000" : "#fff"} />
+                  </Center>
+                </Box>
+              </Box>
             </DrawerFooter>
           </DrawerContent>
         </DrawerOverlay>
