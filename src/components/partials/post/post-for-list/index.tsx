4fc7bd3784ac65374481c3d3ee31298396b4a21a
Ryo Ando <sasigume@gmail.com>
2021-03-18 21:12:55 +0900

    (v2.7.7) added thumbnail for post

--- src/components/partials/post/post-for-list/index.tsx
+++ src/components/partials/post/post-for-list/index.tsx
@@ -3,24 +3,28 @@ import { Badge, Box, Center, Flex } from '@chakra-ui/react'
 import { Post } from '@/models/contentful/Post'
 
 import LinkChakra from '@/components/common/link-chakra'
+import Image from 'next/image'
 
 interface Props {
   post: Post
 }
 export function PostForList({ post }: Props) {
   return (
-    <Flex rounded="xl" shadow="lg" p={3} alignItems="center" area-label={post.title}>
-      <Box flexGrow={1}>
-        <Box textStyle="h4" mb={2}>
-          <LinkChakra href={`/${post.slug}`}>
+    <LinkChakra href={`/${post.slug}`}>
+      <Flex rounded="xl" shadow="lg" p={3} alignItems="center" area-label={post.title}>
+        <Box flexGrow={1}>
+          {post.heroImage && <Center mb={2}><img src={post.heroImage.url} width="full" height="auto" /></Center>}
+          <Box textStyle="h4" mb={2}>
+
             {post.title}
-          </LinkChakra>
+
+          </Box>
+          <Box area-label="更新日時">
+            <Badge colorScheme="blue">公開: {dayjs(post.publishDate ?? post.sys.firstPublishedAt).format('YYYY/MM/DD')}</Badge>
+            <Badge colorScheme="green">最終更新: {dayjs(post.sys.publishedAt).format('YYYY/MM/DD')}</Badge>
+          </Box>
         </Box>
-        <Box area-label="更新日時">
-          <Badge colorScheme="blue">公開: {dayjs(post.publishDate ?? post.sys.firstPublishedAt).format('YYYY/MM/DD')}</Badge>
-          <Badge colorScheme="green">最終更新: {dayjs(post.sys.publishedAt).format('YYYY/MM/DD')}</Badge>
-        </Box>
-      </Box>
-    </Flex>
+      </Flex>
+    </LinkChakra>
   )
 }
