3f506db29ef3434698da3b64da18644805306138
Ryo Ando <sasigume@gmail.com>
2021-03-19 22:45:10 +0900

    (v2.8.0) added commit link in footer

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,3 +2,6 @@ export const SITE_NAME = 'ナポアンのマイクラ'
 export const SITE_URL = 'napoan.com'
 export const CREATOR_ID = 'sasigume'
 export const SITE_DESC = '日本最大のマイクラブログ'
+
+export const VERCEL_LAST_COMMIT = 'https://github.com/' + (process.env.VERCEL_GIT_REPO_OWNER ?? '') + '/' + (process.env.VERCEL_GIT_REPO_SLUG ?? '') + '/commit/' + (process.env.VERCEL_GIT_COMMIT_SHA ?? '')
+export const VERCEL_LAST_COMMIT_MESSAGE = process.env.VERCEL_GIT_COMMIT_MESSAGE ?? ''
