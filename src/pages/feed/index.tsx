bb67d6e4b16b9c31775991e381c27b1f21b423ee
Ryo Ando <sasigume@gmail.com>
2021-03-21 10:29:40 +0900

    generate rss/sitemap more safely

---
+++ src/pages/feed/index.tsx
@@ -0,0 +1,47 @@
+// https://zenn.dev/catnose99/articles/c7754ba6e4adac
+
+import { SITE_DESC, SITE_FULL_URL, SITE_NAME } from '@/lib/constants';
+import { getAllPostsWithSlug } from '@/lib/contentful/graphql';
+import { GetServerSidePropsContext } from 'next'
+import RSS from 'rss';
+
+const TOTAL_LIMIT = parseInt(process.env.TOTAL_PAGINATION ?? '600')
+
+async function generateFeedXml() {
+  const feed = new RSS({
+    title: SITE_NAME,
+    description: SITE_DESC,
+    site_url: SITE_FULL_URL,
+    feed_url: SITE_FULL_URL + '/feed/',
+    language: 'ja',
+  });
+
+  const posts = await getAllPostsWithSlug(false, TOTAL_LIMIT) ?? []
+  posts?.forEach((post) => {
+    feed.item({
+      title: post.title,
+      description: post.description ?? '',
+      author: post.person?.displayName ?? '',
+      date: new Date(post.sys.firstPublishedAt),
+      url: `${process.env.HTTPS_URL}/${post.slug}/`,
+    })
+  })
+
+  return feed.xml({ indent: true })
+}
+
+export const getServerSideProps = async ({ res }: GetServerSidePropsContext) => {
+  const xml = await generateFeedXml()
+
+  res.statusCode = 200;
+  res.setHeader('Cache-Control', 's-maxage=14400, stale-while-revalidate')
+  res.setHeader('Content-Type', 'text/xml')
+  res.end(xml)
+
+  return {
+    props: {},
+  }
+}
+
+const Page = () => null
+export default Page
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,6 @@ import { useRouter } from 'next/router'
 import { Box, Container, Divider, useColorMode, VStack } from '@chakra-ui/react'
 import Layout from '@/components/partials/layout'
 import { getAllPlatformsWithSlug, getAllPostsWithSlug } from '../lib/contentful/graphql'
-import publishRss from '@/lib/rss'
-import publishSitemap from '@/lib/sitemap'
 import { SITE_DESC, SITE_NAME, SITE_URL } from '@/lib/constants'
 import Loading from '@/components/common/loading'
 import { Post } from '@/models/contentful/Post'
@@ -15,9 +13,10 @@ import PostList from '@/components/partials/post'
 import { BreakpointContainer } from '@/components/common/breakpoint-container'
 import { Pagination } from '@/components/common/pagenation'
 import { Platform } from '@/models/contentful/Platform'
-import publishAdsTxt from '@/lib/adsense'
+import publishAdsTxt from '@/lib/adstxt'
 import Image from 'next/image'
 import HeroWithThumbnails from '@/components/common/hero-with-thumbnails'
+import publishRobotsTxt from '@/lib/robotstxt'
 interface IndexProps {
   posts: Post[];
   totalCount: number;
@@ -82,14 +81,12 @@ export async function getStaticProps({ preview = false }) {
 
 
   const allPostsForIndex = (await getAllPostsWithSlug(false, PER_PAGE)) || []
-  // Write only published post into RSS/Sitemap
   const allPostsPublished = (await getAllPostsWithSlug(false, TOTAL_LIMIT)) || []
 
-  publishRss(allPostsPublished)
-  publishSitemap(allPostsPublished)
+  const revalEnv = parseInt(process.env.REVALIDATE ?? '1800')
 
-  // adsense ads.txt
   publishAdsTxt()
+  publishRobotsTxt()
 
   return {
     props: {
@@ -99,7 +96,6 @@ export async function getStaticProps({ preview = false }) {
       preview: preview ?? null,
       allPlatforms: allPlatforms ?? null
     },
-    // Index shouldn't update so often because of rss/sitemap
-    revalidate: 14400
+    revalidate: revalEnv
   }
 }
