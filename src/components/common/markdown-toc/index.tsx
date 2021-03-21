6a90e8efc5c0bf46d03f05a3b6702af4251be641
Ryo Ando <sasigume@gmail.com>
2021-03-21 19:53:57 +0900

    added tweet button to left menu

--- src/components/common/markdown-toc/index.tsx
+++ src/components/common/markdown-toc/index.tsx
@@ -1,19 +1,43 @@
-import { Badge, Box } from "@chakra-ui/layout"
+import {
+  Badge, Box, Flex, Button,
+} from "@chakra-ui/react"
+import { useRouter } from "next/router"
 import ReactMarkdownHeading from "react-markdown-heading"
+import FaiconDiv from "../faicon-div"
+import LinkChakra from "../link-chakra"
 import markdownTocStyles from './markdown-toc-styles.module.css'
+import leftfixedStyles from './leftfixed-styles.module.css'
 
 interface TocProps {
   markdown: string
   headingDepth?: any
+  tweetCount?: number
+  tweetText?: string
 }
 
 const MarkdownToc = (props: TocProps) => {
+  const { asPath } = useRouter()
+  const shareUrl = process.env.HTTPS_URL + asPath as string
+
+  const tweetUrl = `https://twitter.com/share?url=${encodeURIComponent(shareUrl)}&text=${encodeURIComponent(props.tweetText ?? '')}&hashtags=PAWAAAPP`
+
   return (
-    <div className={markdownTocStyles['toc']}>
-      <Box textStyle="h2" mb={4}>目次</Box>
-      <ReactMarkdownHeading headingDepth={props.headingDepth ?? 2} markdown={props.markdown} hyperlink />
-      <Badge colorScheme="red">※Markdownの見出しにのみ対応しています</Badge>
-    </div>
+    <Box>
+      <Flex mb={4} alignItems="center">
+        <Box mr={2}>
+          <Button aria-label="ツイートする" target="_blank" as={LinkChakra} href={tweetUrl} colorScheme="twitter">
+            <FaiconDiv icon={['fab', 'twitter']} />
+          </Button>
+        </Box>
+        <Box className={leftfixedStyles['leftfixed']} fontSize="lg">
+          {props.tweetCount ?? 0}
+        </Box>
+      </Flex>
+      <div className={markdownTocStyles['toc']}>
+        <ReactMarkdownHeading headingDepth={props.headingDepth ?? 2} markdown={props.markdown} hyperlink />
+        <Badge color="black">※Markdownの見出しにのみ対応しています</Badge>
+      </div>
+    </Box>
   )
 }
 
--- src/components/partials/layout/left-fixed/index.tsx
+++ src/components/partials/layout/left-fixed/index.tsx
@@ -1,3 +1,4 @@
+import { useColorMode } from '@chakra-ui/color-mode'
 import { Box, Center } from '@chakra-ui/layout'
 import { ReactNode } from 'react'
 
@@ -12,7 +13,7 @@ export default function LeftFixed({ children }: Props) {
   return (
     <>
       <Center display={{ base: "none", xl: "flex" }} w="18rem" position="fixed" top={0} bottom={0} left={0}>
-        <Box zIndex={5}  shadow="lg" roundedRight="xl" w="full" p={3} bg="gray.100">{children}</Box>
+        <Box color="black" zIndex={5}  shadow="lg" roundedRight="xl" w="full" p={3} bg="gray.100">{children}</Box>
       </Center>
     </>
   )
