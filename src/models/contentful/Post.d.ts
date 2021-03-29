4086dff38c39fefef88e0612f49b4c62c351fcf9
Ryo Ando <sasigume@gmail.com>
2021-03-29 18:26:55 +0900

    added simple `PostBase` model for post list

--- src/models/contentful/Post.d.ts
+++ src/models/contentful/Post.d.ts
@@ -1,16 +1,18 @@
 import { Person } from './Person';
 import { Platform } from './Platform';
 import { Sys } from './Sys';
-
-export interface Post {
+export interface PostBase {
   sys: Sys;
   title: string;
   slug: string;
   publishDate?: string;
-  description?: string;
   heroImage?: {
     url: string;
   };
+}
+
+export interface Post extends PostBase {
+  description?: string;
   body: string;
   person?: Person;
   platformsCollection?: {
