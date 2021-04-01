cade8001d03236e38a55eafd834f7d0f5065d160
Ryo Ando <sasigume@gmail.com>
2021-04-01 14:06:34 +0900

    removed unused dependencies

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,7 +1,6 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 
-declare module 'classnames';
 declare module 'react-gtm-module';
 declare module 'react-syntax-highlighter';
 declare module 'react-syntax-highlighter/dist/cjs/styles/prism';
