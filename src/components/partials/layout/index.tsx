2d53dda6cdb922c1d6d0170c5ceb399cac94671b
Ryo Ando <sasigume@gmail.com>
2021-04-03 14:15:19 +0900

    splited leftfixed component

--- src/components/partials/layout/index.tsx
+++ src/components/partials/layout/index.tsx
@@ -6,8 +6,7 @@ import LinkChakra from '@/components/common/link-chakra';
 import Meta from './meta';
 import Nav from './nav';
 import { Post } from '@/models/contentful/Post';
-import PostList from '../post';
-import ColorSwitch from './color-switch';
+import LeftFixed from './left-fixed';
 const LayoutFooter = dynamic(() => import('./layout-footer'));
 
 interface LayoutProps {
@@ -71,35 +70,7 @@ export default function Layout({
 
         <Box pt={16}>
           <Flex>
-            <Box
-              display={{ base: 'none', lg: 'flex' }}
-              w="18rem"
-              h="100vh"
-              overflowY="scroll"
-              bg={colorMode == 'light' ? 'gray.100' : 'black'}
-              top={0}
-              bottom={0}
-              left={0}
-              p={3}
-              pt={24}
-              position="fixed"
-              zIndex={5}
-              shadow="lg"
-            >
-              <Box w="full">
-                <ColorSwitch />
-                {leftFixedChildren}
-                {drawerPosts && drawerPosts.length > 0 && (
-                  <Box mt={8}>
-                    <PostList mode="drawer" posts={drawerPosts} />
-                  </Box>
-                )}
-
-                <Button w="full" my={8} colorScheme="blue" as={LinkChakra} href="/contact/">
-                  お問い合わせ
-                </Button>
-              </Box>
-            </Box>
+            <LeftFixed leftFixedChildren={leftFixedChildren} drawerPosts={drawerPosts} />
             <Box ml={{ base: 0, lg: '288px' }} flexGrow={1} as="main" pt={8}>
               {children}
               <LayoutFooter revalidate={revalEnv} />
