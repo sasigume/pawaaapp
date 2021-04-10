a5fc20353c25d83d745bdf03b7dab46e4a8710ac
Ryo Ando <sasigume@gmail.com>
2021-04-10 16:20:36 +0900

    Characters in dark mode is now readable in most case

--- src/components/providers/chakra.tsx
+++ src/components/providers/chakra.tsx
@@ -1,11 +1,4 @@
-// e.g. src/Chakra.js
-// a) import `ChakraProvider` component as well as the storageManagers
-import {
-  ChakraProvider,
-  cookieStorageManager,
-  localStorageManager,
-  extendTheme,
-} from '@chakra-ui/react';
+import { ChakraProvider, cookieStorageManager, localStorageManager } from '@chakra-ui/react';
 import { ReactNode } from 'react';
 import { theme } from '@/lib/chakra/styles';
 interface Props {
@@ -13,7 +6,6 @@ interface Props {
   children: ReactNode;
 }
 export function Chakra({ cookies, children }: Props) {
-  // b) Pass `colorModeManager` prop
   const colorModeManager =
     typeof cookies === 'string' ? cookieStorageManager(cookies) : localStorageManager;
   return (
@@ -22,12 +14,9 @@ export function Chakra({ cookies, children }: Props) {
     </ChakraProvider>
   );
 }
-// also export a reusable function getServerSideProps
 export function getServerSideProps({ req }: any) {
   return {
     props: {
-      // first time users will not have any cookies and you may not return
-      // undefined here, hence ?? is necessary
       cookies: req.headers.cookie ?? '',
     },
   };
