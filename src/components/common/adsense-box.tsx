24d6ff4ae5d26b8e4310863504a7f75a867293d7
Ryo Ando <sasigume@gmail.com>
2021-04-13 11:42:13 +0900

    testing react-adsense

--- src/components/common/adsense-box.tsx
+++ src/components/common/adsense-box.tsx
@@ -2,7 +2,7 @@ import { MAIN_WIDTH } from '@/lib/chakra/styles';
 import { Box } from '@chakra-ui/layout';
 import { useRouter } from 'next/router';
 import React, { useEffect } from 'react';
-
+import Adsense from 'react-adsense';
 interface AdsenseProps {
   slot: string;
   path?: string;
@@ -10,7 +10,7 @@ interface AdsenseProps {
 }
 
 // https://qiita.com/qrusadorz/items/14972b6e069feaf777a9
-export default function AdsenseBox({ slot, minWidth }: AdsenseProps) {
+/* export default function AdsenseBox({ slot, minWidth }: AdsenseProps) {
   const { asPath } = useRouter();
 
   //const enableAd = process.env.ENABLE_AD ?? false;
@@ -54,6 +54,29 @@ export default function AdsenseBox({ slot, minWidth }: AdsenseProps) {
           data-full-width-responsive="true"
         ></ins>
       </div>
+    </Box>
+  );
+}*/
+
+export default function AdsenseBox({ slot, minWidth }: AdsenseProps) {
+  const { asPath } = useRouter();
+
+  return (
+    <Box
+      textAlign="center"
+      className="adWrapper"
+      key={asPath}
+      minWidth={`${minWidth ?? 320}px`}
+      minH="250px"
+      maxWidth={`${MAIN_WIDTH}px`}
+      mx="auto"
+      my={4}
+    >
+      <Adsense.Google
+        client={process.env.GOOGLE_AD_CLIENT}
+        slot={slot}
+        style={{ display: 'block', textAlign: 'center' }}
+      />
     </Box>
   );
 }
