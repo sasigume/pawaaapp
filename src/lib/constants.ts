e5bb2f076c6eb932fd347f608bf1ea7980cf0a3b
Ryo Ando <sasigume@gmail.com>
2021-04-15 01:27:15 +0900

    now returned to normal edition

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,7 @@
-export const SITE_NAME = 'ナポアンのマイクラNEXT';
+export const SITE_NAME = 'ナポアンのマイクラ';
 export const SITE_URL = 'napoan.com';
 export const CREATOR_ID = 'sasigume';
-export const SITE_DESC = '読み込み速度のマインクラフト情報サイト';
+export const SITE_DESC = '日本最大のマインクラフト情報サイト';
 export const SITE_FULL_URL =
   process.env.HTTPS_URL ?? 'https://' + process.env.VERCEL_URL ?? 'https://example.com';
 
