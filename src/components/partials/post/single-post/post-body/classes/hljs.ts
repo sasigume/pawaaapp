f957e355e9fbf5c69e83ffebcd7a68f556455420
Ryo Ando <sasigume@gmail.com>
2021-04-20 20:51:58 +0900

    splitted hljs class

---
+++ src/components/partials/post/single-post/post-body/classes/hljs.ts
@@ -0,0 +1,13 @@
+export const hljs = {
+  'pre.hljs': {
+    borderRadius: '6px',
+    background: '#222',
+    fontFamily: "Consolas, 'Courier New', Courier, Monaco, monospace, Noto Sans",
+    margin: '0px 0px 8px',
+  },
+  'pre.hljs .highlightline': {
+    background: '#7b4000',
+    display: 'inline-block',
+    width: '100%',
+  },
+};
--- src/components/partials/post/single-post/post-body/classes/index.ts
+++ src/components/partials/post/single-post/post-body/classes/index.ts
@@ -4,3 +4,4 @@ export { mcVersions } from './mc-versions';
 export { commandBlock } from './command-block';
 export { updateContent } from './update-content';
 export { youtubeBox } from './youtube-box';
+export { hljs } from './hljs';
